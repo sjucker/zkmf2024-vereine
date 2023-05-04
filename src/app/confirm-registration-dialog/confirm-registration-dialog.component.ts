7fd4dbfc71c69e6236b8595d42dac9aa41f02e5c
Stefan Jucker <stefan.jucker@gmail.com>
2023-05-04 22:11:47 +0200

    Registration confirmation

---
+++ src/app/confirm-registration-dialog/confirm-registration-dialog.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-registration-dialog',
+  templateUrl: './confirm-registration-dialog.component.html',
+  styleUrls: ['./confirm-registration-dialog.component.scss']
+})
+export class ConfirmRegistrationDialogComponent {
+
+}
