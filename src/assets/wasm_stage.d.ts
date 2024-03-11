85380ca6b27ee2a85214632eed13113c0fd9064d
Balazs Sandor Mracsko <4477623+mracsko@users.noreply.github.com>
2024-03-11 20:17:14 +0100

    Adding first draft of stage planner

---
+++ src/assets/wasm_stage.d.ts
@@ -0,0 +1,4 @@
+export function read_data(): string;
+export function set_data(data: string): void;
+export function add(canvas_id: string, data: string): void;
+export default function init(input: string): void;
