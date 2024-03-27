fbcf2d0702b0510fc3d58c3f4b37dc35097de1da
Stefan Jucker <stefan.jucker@gmail.com>
2024-03-27 20:50:58 +0100

    Update stager.wasm, add link to PDF

---
+++ src/assets/stage/wasm_stage.d.ts
@@ -0,0 +1,15 @@
+export function read_data(): string;
+
+export function read_data_for_save(): string;
+
+export function set_data(data: string): void;
+
+export function add_viewer(canvas_id: string, stage_width: number, location_id: string, data: string): string;
+
+export function add_editor(canvas_id: string, stage_width: number, location_id: string, data: string): string;
+
+export function set_debug_mode(): void;
+
+export function is_data_dirty(): boolean;
+
+export default function init(input: string): Promise<unknown>;
