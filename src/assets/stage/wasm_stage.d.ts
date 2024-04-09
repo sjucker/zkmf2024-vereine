29559bc2ef64bcf842aea170d84cb927d06ed374
Stefan Jucker <jucker@mptechnology.ch>
2024-04-09 20:23:54 +0200

    Update stager.wasm

--- src/assets/stage/wasm_stage.d.ts
+++ src/assets/stage/wasm_stage.d.ts
@@ -4,7 +4,7 @@ export function read_data_for_save(): string;
 
 export function set_data(data: string): void;
 
-export function add_viewer(canvas_id: string, stage_width: number, location_id: string, data: string): string;
+export function add_viewer(canvas_id: string, stage_width: number, location_id: string, data: string, print_view: boolean): string;
 
 export function add_editor(canvas_id: string, stage_width: number, location_id: string, data: string): string;
 
